3dfce6ff2513c9dfc231293b84f66d9ba600947d
Trevor <tlist403@gmail.com>
2022-01-01 16:51:45 -0300

    New book updates display

--- js/script.js
+++ js/script.js
@@ -45,6 +45,7 @@
 
       
       testing();
+
     }
        else { 
         //content.textContent = 'The button was triggered'
@@ -62,10 +63,39 @@
     const testTarget = document.getElementById('test-button');
     testButton.addEventListener("click", function () {
       testTarget.textContent = document.getElementById('author').value;
+  addBooksToLibrary();
+
     });
   }
   function addBooksToLibrary() {
-    
+    const targetTitle = document.getElementById('title');
+
+      const targetAuthor = document.getElementById('author');
+    const targetPages = document.getElementById('page_numbers');
+
+    if (targetTitle && targetAuthor && targetPages) {
+    document.getElementById('card_parent').remove();
+
+    let read_status_value;
+
+      let targetNotCompleted = document.getElementById('not_completed'); 
+      let targetCompleted = document.getElementById('completed');
+
+      if (targetNotCompleted.checked) {
+        read_status_value = targetNotCompleted.value;
+      } else {
+        read_status_value = targetCompleted.value;
+      }
+      console.log(myLibrary)
+
+      // if (targetTitle.value && targetAuthor.value && targetPages.value) {
+
+        let newBook = new Book(targetTitle.value, targetAuthor.value, targetPages.value, read_status_value)
+        myLibrary.push(newBook);
+  displayLibrary();
+
+      // }
+    }
   }
 
   function displayLibrary() {
@@ -85,24 +115,26 @@
       divMaker.innerHTML = `Title: ${myLibrary[i]['title']}` + '<br><hr>' + `Author: ${myLibrary[i]['author']}` + '<br><hr>' + `Pages: ${myLibrary[i]['pages']}`+ '<br><hr>' + `Read Status: ${myLibrary[i]['read_status']}` ;
       document.getElementById('card_parent').appendChild(divMaker);
     }
-      parentDiv.children[1].style.backgroundColor = 'red';
+      //parentDiv.children[1].style.backgroundColor = 'red';
   }
 
 
-
-  let initialBook = new Book("The Hobbit", "J. R. R. Tolkien", 304, "Completed");
+  function initializeLibrary() {
+    let initialBook = new Book("The Hobbit", "J. R. R. Tolkien", 304, "Completed");
   let newBook2 = new Book("Angels and Demons", "Dan Brown", 616, "Not completed");
   let newBook3 = new Book("The Miserable Mill", "Lemony Snicket", 195, "Completed");
 
   myLibrary.push(initialBook);
   myLibrary.push(newBook2);
   myLibrary.push(newBook3);
+  }
+  
 
 
   
-
+  initializeLibrary();
   newBookButtonToggle();
+  addBooksToLibrary();
   displayLibrary();
-  //testing();
   document.getElementById('test-button').style.color = "red"
 })();
